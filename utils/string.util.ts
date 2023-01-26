c4ed45d2fe92311aa7f46a60e5d65551f4d94634
김소연 <sykim@blockodyssey.io>
2023-01-26 14:05:27 +0900

    fix: makeSortString 로직 수정

--- utils/string.util.ts
+++ utils/string.util.ts
@@ -3,10 +3,18 @@ export class StringUtils {
     let sortString =
       str === '곧 도착'
         ? '0'
-        : str.split('[')[0].replace('분', '').replace('초후', '');
-    if (sortString !== '0' && sortString.length !== 3) {
+        : str
+            .split('[')[0]
+            .replace('분', '')
+            .replace('초후', '')
+            .replace('후', '');
+    if (sortString !== '0' && sortString.length === 1) {
+      sortString = sortString + '00';
+    }
+    if (sortString.length === 2) {
       sortString = [sortString.slice(0, 1), '0', sortString.slice(1)].join('');
     }
+    console.log(sortString);
     return Number(sortString);
   }
 }
